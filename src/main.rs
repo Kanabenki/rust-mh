1e63b126ec303369a6e9ba1aa07fb29e8e6e3ab4
midiphony <midiphony@gmail.com>
2019-05-14 10:28:24 +0200

    Add first version of Solution class

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,21 @@ type NodeId = u64;
 struct GraphOut { nodes: Vec<NodeId>, edges: Vec<(NodeId, NodeId)> }
 
 struct Task {
-    name: String,
-    
+    node_id: u64,
+
+    evac_rate: u64,
+
+    start_offset: u64,
+
     is_valid: bool,
+
+    /*
+        A solution file only gives the three informations above
+        So we need the original graph to evaluate the time_length of a task
+    */
+
+    // time_length = nb_of_people / evac_rate
+    // time_length: u64
 }
 
 struct Ressource {
@@ -123,6 +135,109 @@ impl Graph {
         dot::render(&g, w);
     }
 }
+
+
+
+/*
+
+    SOLUTION_CHECKER :
+    (opt)0. Parsing du fichier solution
+    1. A l'ajout d'une tâche :
+        1.a. L'ajouter aux ressources, dans l'ordre, pour tenir compte du décalage
+            1.a.a. Pour chaque ressource, vérifier que la capacité n'est pas dépassé
+
+    Ca donnait en python : 
+    
+    self.instance_name = instance_name
+    self.evac_nb = evac_nb
+    self.evac_nodes = evac_nodes
+    self.is_valid = is_valid
+    self.obj_func_value = obj_func_value
+    self.eval_time = eval_time
+    self.method_name = method_name
+    self.comment = comment
+
+
+*/
+
+struct Solution {
+    graph_name: String,
+    nb_of_evac_nodes: u64,
+    tasks: Vec<Task>,
+    is_valid: bool,
+    obj_value: u64,
+    evaluation_time: u64,
+    method: String,
+    comment: String,
+}
+
+impl Solution {
+    fn new() -> Self {
+        Solution {
+            graph_name : String::new(),
+            nb_of_evac_nodes : 0,
+            tasks : Vec::new(),
+            is_valid : false,
+            obj_value : 0,
+            evaluation_time : 0,
+            method : String::new(),
+            comment : String::new(),
+        }
+    }
+
+    fn parse(r: impl BufRead) -> Result<Solution, ()> {
+
+        let mut sol = Solution::new();
+
+        let mut lines = r.lines().map(|l| l.expect("Could not read line"));
+
+        let graph_name = lines.next();
+        sol.graph_name = graph_name.expect("");
+
+        let nb_of_evac_nodes = scan_fmt!(&lines.next().unwrap(), "{}", u64).unwrap();
+        sol.nb_of_evac_nodes = nb_of_evac_nodes;
+
+        for _ in 0..nb_of_evac_nodes {
+
+            let (node_id_to_evac, evac_rate, start_offset) = scan_fmt!(&lines.next().unwrap(), "{} {} {}", u64, u64, u64).unwrap();
+
+            let mut task = Task{
+                node_id: node_id_to_evac,
+
+                // It looks like "is_valid" in a task is useless
+                is_valid: false,
+
+                evac_rate: evac_rate,
+                start_offset: start_offset,
+            };
+
+            sol.add_task(task);
+
+        }
+
+        sol.is_valid = lines.next().unwrap().trim() == "valid";
+
+        sol.obj_value = scan_fmt!(&lines.next().unwrap(), "{}", u64).unwrap();
+
+        sol.evaluation_time = scan_fmt!(&lines.next().unwrap(), "{}", u64).unwrap();
+
+        sol.method = lines.next().unwrap();
+
+        sol.comment = lines.next().unwrap();
+
+        Ok(sol)
+        
+    }
+    
+    fn add_task(&mut self, task: Task) {
+        self.tasks.push(task);
+    }
+}
+
+
+
+
+
 
 impl<'a> dot::Labeller<'a, NodeId, (NodeId, NodeId)> for GraphOut {
     fn graph_id(&'a self) -> dot::Id<'a> { dot::Id::new("Graph").unwrap() }
